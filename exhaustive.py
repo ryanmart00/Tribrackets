124e8abc413225755c6f0260b8a8c883195bec1c
Ryan Martinez <ryanmart00@gmail.com>
2021-10-08 12:24:21 -0700

    made exhaustive more exhaustive

--- exhaustive.py
+++ exhaustive.py
@@ -4,6 +4,6 @@ from tribracket_conditions import *
 if __name__ == "__main__":
     for n in count(3):
         t = Totient(n)
-        for d in range(1, t-1):
+        for d in range(1, 2*(t-1)):
             print("Polynomials of degree %s on Z/%sZ (with totient %s)" % (d,n,t), flush=True)
             compute(Z(n),d)
